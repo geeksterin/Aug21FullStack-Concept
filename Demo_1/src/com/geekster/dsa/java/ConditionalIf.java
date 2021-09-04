37f6327a50298004f6188bd99bb69947e60fc7f4
Akshay <limayeakshay404@gmail.com>
2021-09-04 16:44:44 +0530

    Greater number

---
+++ Demo_1/src/com/geekster/dsa/java/ConditionalIf.java
@@ -0,0 +1,21 @@
+package com.geekster.dsa.java;
+
+public class ConditionalIf {
+
+	public static void main(String[] args) {
+		
+		int num1 = 123;
+		
+		int num2 = 1286;
+		
+		if(num1 > num2) {
+			System.out.println(num1);
+		}else {
+			System.out.println(num2);
+		}
+		
+		
+		
+	}
+	
+}
---
+++ Demo_1/src/com/geekster/dsa/java/MinNumber.java
@@ -0,0 +1,18 @@
+package com.geekster.dsa.java;
+
+public class MinNumber {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 9;
+		int num2 = 124;
+		
+		if(num1 > num2) {
+			System.out.print(num2);
+		}else {
+			System.out.print(num1);
+		}
+		
+	}
+
+}
